6a6af472dc8ad0ac0205a3066913fc86b2f0d05f
Isaac Smith <isaacsmith215@aol.com>
2020-03-14 17:48:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,11 @@
 def prime?(num)
   i = 2
   if num > 1 
-    array
+    numtest = (i..num - 1).to_a
+    numtest.none? do |number|
+      num % number == 0
+    end
+  else
+    return false
+  end
+end
