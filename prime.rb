0d46d8caf331f0c2a795ddb3ffd7f84bc7280f50
Isaac Smith <isaacsmith215@aol.com>
2020-03-14 17:44:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,4 @@
-def prime?(array)
-  i = 0
-  while i < array.length
-    if array[i] % 2 == 1 && array[i]
-      return true
-    else 
-      return false
-    end
-  end
-end
+def prime?(num)
+  i = 2
+  if num > 1 
+    array
