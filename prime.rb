882945099661ffe15e1f3ac4d7d83b2c9faec9c0
Isaac Smith <isaacsmith215@aol.com>
2020-03-14 17:48:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  i = 2
+  if num > 1 
+    numtest = (i..num - 1).to_a
+    numtest.none? do |number|
+      num % number == 0
+    end
+  else
+    return false
+  end
+end
